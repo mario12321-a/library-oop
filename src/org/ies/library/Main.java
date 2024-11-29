5f4c913917b5b5aed03daec8c7f827b9d49055e0
mario <vlad10_123@hotmail.com>
2024-11-29 12:36:41 +0100

    completo

--- src/org/ies/library/Main.java
+++ src/org/ies/library/Main.java
@@ -1,7 +1,19 @@
 package org.ies.library;
 
+import org.ies.library.componentes.AutorReader;
+import org.ies.library.componentes.BibliotecaReader;
+import org.ies.library.componentes.BookReader;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        var Scanner = new Scanner(System.in);
+        var autorReader = new AutorReader(Scanner);
+        var bookReader = new BookReader (Scanner, autorReader);
+        var bibliotecaReader = new BibliotecaReader(Scanner, bookReader);
 
+        var biblioteca = bibliotecaReader.read();
+        System.out.println(biblioteca);
     }
 }
